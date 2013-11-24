bfab6e3bd3d9c18b30b5c69124361b7c11890fe1
Kasper Peulen <kasperpeulen@gmail.com>
2013-11-24 22:05:48 +0100

    added custom $$...$$ that can be used as align

--- assets/javascripts/tex_dialect.js
+++ assets/javascripts/tex_dialect.js
@@ -19,11 +19,10 @@ Discourse.Dialect.replaceBlock({
   emitter: function(contents) { return '\\['+contents+'\\]';  }
 });
 
-Discourse.Dialect.replaceBlock({
-  start: /(\$\$)([\s\S]*)/,
-  stop: '$$',
-  rawContents: true,
-  emitter: function(contents) { return '$$'+contents+'$$';  }
+Discourse.Dialect.inlineRegexp({
+  start: /\$\$/,
+  matcher: /(\$\$)([\S\s]+)(\$\$)/,
+  emitter: function(matches) { return matches[0]; }
 });
 
 Discourse.Dialect.inlineRegexp({
@@ -31,3 +30,18 @@ Discourse.Dialect.inlineRegexp({
   matcher: /(\\begin{[\S\s]+})([\S\s]*)(\\end{[\S\s]+})/,
   emitter: function(matches) { return matches[0]; }
 });
+
+/**
+This is a function that allows $$..$$ to be used as align if a & is typed.
+Usefull for lazy latex typers like me, it works like shortcut.
+
+Discourse.Dialect.inlineRegexp({
+  start: /\$\$/,
+  matcher: /(\$\$)([\S\s]+)(\$\$)/,
+  emitter: function(matches) { 
+	if ( matches[2].match(/[\S\s]+[&][\S\s]+/) ){
+		return '\\begin{align*}'+matches[2]+'\\end{align*}'; }
+	else {return matches[0];}
+	}
+});
+**/
