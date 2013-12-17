b1a2e8cdd70fa06bfed7ddf3cd6946dafffee99a
Kasper Peulen <kasperpeulen@gmail.com>
2013-12-17 15:38:37 +0100

    0.4.1

--- mathjax/plugin.rb
+++ mathjax/plugin.rb
@@ -1,47 +1,69 @@
 # name: MathJax support for Discourse
-# version: 0.4
+# version: 0.4.1
 # authors: Sam Saffron, Kasper Peulen
 
 register_asset('javascripts/tex_dialect.js', :server_side)
 register_javascript <<JS 
 
-    Discourse.addInitializer(function () {
+  Discourse.addInitializer(function () {
 
-      $LAB.script('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML').wait(function() {
+    $LAB.script('http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML').wait(function () {
 
-MathJax.Hub.Config({ "HTML-CSS": { 
-	preferredFont: "TeX", 
-	availableFonts: ["TeX"], 
-	linebreaks: { automatic:true }, 
-	EqnChunk: (MathJax.Hub.Browser.isMobile ? 10 : 50) },
-        tex2jax: { 
-	inlineMath: [ ["$", "$"], ["\\\\(","\\\\)"] ], 
-	displayMath: [ ["$$","$$"], ["\\\\[", "\\\\]"] ], 
-	processEscapes: true},
-        TeX: { 
-	noUndefined: { attributes: { mathcolor: "red", mathbackground: "#FFEEEE", mathsize: "90%" } }, 
-	Macros: { href: "{}" } },
+      MathJax.Hub.Config({
+        "HTML-CSS": {
+          preferredFont: "TeX",
+          availableFonts: ["TeX"],
+          linebreaks: {
+            automatic: true
+          },
+          EqnChunk: (MathJax.Hub.Browser.isMobile ? 10 : 50)
+        },
+        tex2jax: {
+          inlineMath: [
+            ["$", "$"],
+            ["\\\\(", "\\\\)"]
+          ],
+          displayMath: [
+            ["$$", "$$"],
+            ["\\\\[", "\\\\]"]
+          ],
+          processEscapes: true
+        },
+        TeX: {
+          noUndefined: {
+            attributes: {
+              mathcolor: "red",
+              mathbackground: "#FFEEEE",
+              mathsize: "90%"
+            }
+          },
+          Macros: {
+            href: "{}"
+          }
+        },
         messageStyle: "none"
-});
-
-        var applyPreview = function(){
-          MathJax.Hub.Queue(["Typeset",MathJax.Hub,"wmd-preview"]);
-		  	// if the caret is on the last line ensure preview scrolled to bottom
-			var caretPosition = Discourse.Utilities.caretPosition(this.wmdInput[0]);
-			if (!this.wmdInput.val().substring(caretPosition).match(/\n/)) {
-			var $wmdPreview = $('#wmd-preview');
-			if ($wmdPreview.is(':visible')) {
-			$wmdPreview.scrollTop($wmdPreview[0].scrollHeight);
-        };
-
-        var applyBody = function(){
-          MathJax.Hub.Queue(["Typeset",MathJax.Hub,"topic"]);
-        };
-
-        Discourse.PostView.prototype.on("postViewInserted", applyBody);
-        Discourse.ComposerView.prototype.on("previewRefreshed", applyPreview);
-
       });
 
+      var applyPreview = function () {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, "wmd-preview"]);
+        // if the caret is on the last line ensure preview scrolled to bottom
+        var caretPosition = Discourse.Utilities.caretPosition(this.wmdInput[0]);
+        if (!this.wmdInput.val().substring(caretPosition).match(/\\n/)) {
+          var $wmdPreview = $('#wmd-preview');
+          if ($wmdPreview.is(':visible')) {
+            $wmdPreview.scrollTop($wmdPreview[0].scrollHeight);
+          }
+        }
+      };
+
+      var applyBody = function () {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, "topic"]);
+      };
+
+      Discourse.PostView.prototype.on("postViewInserted", applyBody);
+      Discourse.ComposerView.prototype.on("previewRefreshed", applyPreview);
+
     });
+
+  });
 JS
